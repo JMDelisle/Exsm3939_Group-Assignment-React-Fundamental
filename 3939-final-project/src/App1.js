1976d34bf75d91e23b99a9adf5998d853bcf0abc
Corey Woodcox <ctwoodcox@outlook.com>
2022-06-02 02:48:09 -0600

    Cleaned up duplicate files, added flip functionality, added css card images and edited color theme, modified app.js

--- 3939-final-project/src/App1.js
+++ 3939-final-project/src/App1.js
@@ -1,46 +1,80 @@
 import './App.css';
-import './MemoryCards';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import SingleCard from './components/SingleCard';
 
 const images = [
-  {"src": "/img/catsmiling.jpg" },
-  {"src": "/img/cooldog.jpg"},
-  {"src": "/img/dogsmiling.jpg"},
-  {"src": "/img/hellothere.jpg"},
-  {"src": "/img/monkeybananas.jpg"},
-  {"src": "/img/oneeyemonkey.jpg"}
+  { "src": "/img/audi.jpg", matched: false },
+  { "src": "/img/bmw.jpg", matched: false },
+  { "src": "/img/ferrari.jpg", matched: false },
+  { "src": "/img/ford.jpg", matched: false },
+  { "src": "/img/mercedes.jpg", matched: false },
+  { "src": "/img/rangerover.jpg", matched: false }
 ]
 
 
 function App() {
- const [cards, setCards] = useState([])
- const [turns, setTurns] = useState(0)
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+
   // shuffle cards
-  const shuffleCards =() => {
+  const shuffleCards = () => {
     const shuffle = [...images, ...images]
-    .sort(() => Math.random() -0.5)
-    .map((card) => ({ ...card, id: Math.random()}))
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
 
     setCards(shuffle)
     setTurns(0)
   }
-  console.log(cards, turns)
+
+  // Handle a choice.
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // Compare two selected cards.
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // Reset choices & increase turn.
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
   return (
     <div className="App">
-      <h1>Match Game</h1>
+      <h1>"Match My Ride" the Memory Game</h1>
       <button onClick={shuffleCards}>Start New Game</button>
 
       <div class="card-grid">
-        {cards.map(card =>(
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front"/>
-              <img className="back" src="/img/trollface2.jpg" alt="card back"/>
-            </div>
-          </div>
+        {cards.map(card => (
+            <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} />
         ))}
       </div>
-
     </div>
   );
 }

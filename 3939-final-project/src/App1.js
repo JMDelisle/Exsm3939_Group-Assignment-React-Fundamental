e9fd5d1b8d720998a9df019465742a4167aa8036
Corey Woodcox <ctwoodcox@outlook.com>
2022-06-02 22:22:49 -0600

    deleted dispatch from App1.js

--- 3939-final-project/src/App1.js
+++ 3939-final-project/src/App1.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Dispatch, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MemoryCard from './components/MemoryCard';
 import Timer from './Timer';
 
